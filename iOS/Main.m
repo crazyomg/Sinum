283fe55ed25f6c97e8c5bba549b1a59a160467dd
crazyomg <strangeriosbuilds2@gmail.com>
2025-12-01 12:45:43 -0500

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://2.56.244.124:8080"
+#define API_URL @"http://2.56.244.124"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
